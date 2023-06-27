cf861faf7e313cf62c76adb73d934eb8148f6d5b
dmusial98 <dmusial98@gmail.com>
2023-06-27 23:47:46 +0200

    work on load GameHisotry from log file

--- HeartsServer/ResultsWriterReader/TxtFileLogReader.cs
+++ HeartsServer/ResultsWriterReader/TxtFileLogReader.cs
@@ -1,5 +1,6 @@
 ﻿using Hearts_server.ResultsWriterReader;
 using HeartsServer.GameLogic.History;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace HeartsServer.ResultsWriterReader
 {
@@ -24,9 +25,31 @@ namespace HeartsServer.ResultsWriterReader
 
             GameHistory history = new GameHistory();
             string[] linesFromFile = await LoadFromFile(file);
+            var listLines = linesFromFile.ToList();
+            
+            //usun niepotrzebne logi +
+            //zaladuj start gry C3 + 
+            //zaladuj rozdanie kart C4 
+            //zaladuj zaladuj wymianki C5 C6
+            //zaladuj paczke czterech rzuconych kart, wygranie lewy i punkty w rundzie C7 C8 C9
+            //zaladuj karty graczy po lewie C12
+            //po 13 lewach zaladuj punkty po rundzie C10
+            //powtarzaj aż załaduje informację o miejscach C11
+
+            listLines.RemoveAll(IsUnnecessaryLog);
+
+            var startGameLog = listLines.Where(s => s[..3] == "C3 ").ToArray();
+            if (startGameLog.Count() != 1)
+                throw new FileLoadException($"File {file.Name} doesn't contain exactly one start game message log");
+
+            listLines.Remove(startGameLog.First());
+
+            var playerCardsBeforeGameLog = listLines.Where(s => s[..3] == "C4 ").ToArray();
+
+            
 
 
-            return new GameHistory();
+            return history;
         }
 
         private async Task<string[]> LoadFromFile(FileInfo file)
@@ -34,5 +57,11 @@ namespace HeartsServer.ResultsWriterReader
             using (var readTask = File.ReadAllLinesAsync(file.Name))
                 return await readTask;
         }
+
+        private static bool IsUnnecessaryLog(string input)
+        {
+            return input[..3] == "C1 " || input[..3] == "C2 " || input[..3] == "C13";
+        }
+
     }
 }
