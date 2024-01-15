5b81f27732e54d34b81457c9031b5bbca7f3448d
dmusial98 <dmusial98@gmail.com>
2024-01-15 13:42:42 +0100

    Changed Colors and Values names in excel hisotry game file

---
+++ HeartsServer.GameLogic.Tests/Readers.Tests/ExcelFileReaderTest.cs
@@ -0,0 +1,23 @@
+﻿using HeartsServer.ResultsWriterReader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeartsServer.GameLogic.Tests.Readers.Tests
+{
+	[TestClass]
+	public class ExcelFileReaderTest
+	{
+
+		[TestMethod]
+		public void RunExcelChange()
+		{
+			ExcelFileReader reader = new ExcelFileReader();
+			reader.ChangeNameColorsAndValuesInExcel(@"LogFiles\Hearts_history.xlsx");
+
+
+		}
+	}
+}
--- HeartsServer/ResultsWriterReader/ExcelFileReader.cs
+++ HeartsServer/ResultsWriterReader/ExcelFileReader.cs
@@ -15,29 +15,59 @@ namespace HeartsServer.ResultsWriterReader
 		}
 
 
-		public void ChangeNameColorsInExcel(string fileName)
+		public void ChangeNameColorsAndValuesInExcel(string fileName)
 		{
-			using (var package = new ExcelPackage(@"c:\temp\myWorkbook.xlsx"))
+			var fileInfo = new FileInfo(fileName);
+			using (var package = new ExcelPackage(fileInfo))
 			{
-				var sheet = package.Workbook.Worksheets.Add("Round_01");
+				var sheet = package.Workbook.Worksheets["Round_01"];
 
 				var cells = sheet.Cells["B4:N7"];
-
-				foreach (var cell in cells)
-				{
-					string value = cell.Text;
-
-					string[] arr = value.Split(' ');
-
-					value.Replace("kier", "Heart");
-					value.Replace("trefl", "Club");
-					value.Replace("karo", "Diamond");
-					value.Replace("pik", "Spade");
-				}
+				doForeach(cells);
+				cells = sheet.Cells["B10:D13"];
+				doForeach(cells);
+				cells = sheet.Cells["B17:N20"];
+				doForeach(cells);
+				cells = sheet.Cells["B25:E37"];
+				doForeach(cells);
 
 				// Save to file
 				package.Save();
 			}
 		}
+
+		private void doForeach(ExcelRange cells)
+		{
+			foreach (var cell in cells)
+			{
+				string value = cell.Text;
+
+				cell.Value = cell.Text.Replace("kier", "Heart");
+				cell.Value = cell.Text.Replace("trefl", "Club");
+				cell.Value = cell.Text.Replace("karo", "Diamond");
+				cell.Value = cell.Text.Replace("pik", "Spade");
+
+				string[] splitted = cell.Text.Split(' ');
+
+				cell.Value = splitted[0] switch
+				{
+					"2" => "Two" + " " + splitted[1],
+					"3" => "Three" + " " + splitted[1],
+					"4" => "Four" + " " + splitted[1],
+					"5" => "Five" + " " + splitted[1],
+					"6" => "Six" + " " + splitted[1],
+					"7" => "Seven" + " " + splitted[1],
+					"8" => "Eight" + " " + splitted[1],
+					"9" => "Nine" + " " + splitted[1],
+					"10" => "Ten" + " " + splitted[1],
+					"J" => "Jack" + " " + splitted[1],
+					"Q" => "Queen" + " " + splitted[1],
+					"K" => "King" + " " + splitted[1],
+					"A" => "Ace" + " " + splitted[1],
+					_ => cell.Value
+				};
+			}
+		}
+
 	}
 }
