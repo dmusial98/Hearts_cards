80bd00dcc4dd91f1a9d30a1e45dab60f43d38e97
dmusial98 <dmusial98@gmail.com>
2024-01-18 15:27:20 +0100

    Changed excel file with game history

--- HeartsServer.GameLogic.Tests/Readers.Tests/ExcelFileReaderTest.cs
+++ HeartsServer.GameLogic.Tests/Readers.Tests/ExcelFileReaderTest.cs
@@ -16,15 +16,15 @@ namespace HeartsServer.GameLogic.Tests.Readers.Tests
 			ExcelFileReader reader = new ExcelFileReader();
 			var task = reader.GetGameHistoryAsync();
 			task.Wait();
-			
+
 			Assert.IsNotNull(task.Result);
 			Assert.AreEqual(2, task.Result.Rounds.Count);
 			Assert.AreEqual("Five Heart", task.Result.Rounds[0].PlayerCardsBeforeExchange[0].Cards[0].ToString());
-            Assert.AreEqual("Ace Spade", task.Result.Rounds[1].PlayerCardsBeforeExchange[0].Cards[0].ToString());
+			Assert.AreEqual("Ace Spade", task.Result.Rounds[1].PlayerCardsBeforeExchange[0].Cards[0].ToString());
 
-            //reader.WriteCardsBeforeTrickInExcelFile(@"LogFiles\Hearts_history.xlsx");
+			//reader.WriteCardsBeforeTrickInExcelFile(@"LogFiles\Hearts_history.xlsx");
 
-        }
+		}
 
 
 		//[TestMethod]
